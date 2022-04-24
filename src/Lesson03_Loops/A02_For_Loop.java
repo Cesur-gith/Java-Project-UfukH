6040ce878cff03d7f7fcf51a792c269f8579d218
Jasur Artykbayev <jasur90@gmail.com>
2022-04-24 12:51:29 +0200

    First Lesson

---
+++ src/Lesson03_Loops/A02_For_Loop.java
@@ -0,0 +1,19 @@
+package Lesson03_Loops;
+
+public class A02_For_Loop {
+
+    public static void main(String[] args) {
+
+        // Verilen bir sayinin toplami => 5 => 1+2+3+4+5 = 15
+        int toplam = 0;
+        for (int i = 1; i <= 5; i++) {
+            toplam = toplam + i;
+            if (i < 5) {
+                System.out.print(i + "+");
+            } else {
+                System.out.print(i);
+            }
+        }
+        System.out.print("=" + toplam);
+    }
+}
---
+++ src/Lesson03_Loops/A02_For_Loop_BasamaklariAyniOlanSayilar.java
@@ -0,0 +1,9 @@
+package Lesson03_Loops;
+
+public class A02_For_Loop_BasamaklariAyniOlanSayilar {
+
+    public static void main(String[] args) {
+
+    }
+
+}
