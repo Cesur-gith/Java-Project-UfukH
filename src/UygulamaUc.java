c50803d1e4a4f1f664ea9c9e2ace20178b6b6103
Jasur Artykbayev <jasur90@gmail.com>
2022-04-24 03:01:52 +0200

    First Lesson

--- src/UygulamaUc.java
+++ src/UygulamaUc.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class UygulamaUc {
+public class UygulamaUc {
+
+    public static void main(String[] args) {
+
+        int s1 =11, s2 = 2;
+        int bolum = s1 / s2;
+        System.out.println("Bolum :"  + bolum);
+
+        int s3 = 10, s4 = 3;
+        double bolum1 = s3/s4;
+        double bolum2 = (double) s3/ (double) s4;
+        System.out.println("Bolum1 :" + bolum1);
+        System.out.println("Bolum1 :" + bolum2);
+
+
+    }
 }
