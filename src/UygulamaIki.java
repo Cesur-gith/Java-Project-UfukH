ab5596e86af31b581ca343b4e4734b79271fa931
Jasur Artykbayev <jasur90@gmail.com>
2022-04-24 02:45:39 +0200

    First Lesson

--- src/UygulamaIki.java
+++ src/UygulamaIki.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class UygulamaIki {
+public class UygulamaIki {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+        // degisken turu, adi
+
+        int s1 = 5; // degisken turu int tamsayi
+        int s2; // degisken tanimladik ama atama yapmadik. s2 degeri = "NULL" yani hiclik
+        s2 = 3; // burda s2 degerine atama yaptik
+        int toplam = s1 + s2;
+        System.out.println("toplam: " + toplam);
+
+
+
+    }
 }
